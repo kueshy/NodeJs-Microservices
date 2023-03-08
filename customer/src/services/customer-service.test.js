6077ae35bce82cbd69f0919bd17d75915178def9
kueshy <eliasemonyi@yahoo.com>
2023-03-08 11:24:49 +0300

    Added test Cases Configuration

---
+++ customer/src/services/customer-service.test.js
@@ -0,0 +1,10 @@
+// Which service it is
+describe("CustomerService", () => {
+  // which function
+  describe("SignIn", () => {
+    // which scenario we are testing
+    test("validate user inputs", () => {});
+
+    test("validate response", async () => {});
+  });
+});
---
+++ shopping/src/services/shopping-service.test.js
@@ -0,0 +1,10 @@
+// Which service it is
+describe("ShoppingService", () => {
+  // which function
+  describe("PlaceOrder", () => {
+    // which scenario we are testing
+    test("validate user inputs", () => {});
+
+    test("validate response", async () => {});
+  });
+});
